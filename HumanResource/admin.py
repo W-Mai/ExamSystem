0db42291632856b19e33678f79fa09d9e6e0690e
W-Mai <1341398182@qq.com>
2022-04-25 23:44:25 +0800

    增加：配置管理界面

--- HumanResource/admin.py
+++ HumanResource/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+
+@admin.register(Human)
+class HumanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'department', 'attendance_status')
+    list_filter = ('department', 'attendance_status')
+    search_fields = ('name', 'department')
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(AttendanceStatus)
+class AttendanceStatusAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
