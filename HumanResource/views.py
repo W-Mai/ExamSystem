2f18866f9459a9f2237cd420330c4bc9858a87f5
W-Mai <1341398182@qq.com>
2022-04-30 14:20:36 +0800

    gitflow-feature-stash: 测试页面

--- HumanResource/views.py
+++ HumanResource/views.py
@@ -1,3 +1,16 @@
+import time
+
 from django.shortcuts import render
+import random
+
 
 # Create your views here.
+def home(request):
+    name = request.session.get('name', None)
+    if name is None:
+        name = str(time.time())
+        request.session['name'] = name
+
+    return render(request, 'home.html', {
+        'name': name,
+    })
